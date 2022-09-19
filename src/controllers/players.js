84afaa85c6ccaa3b2559e4023f55655bdf281bb5
Wesley Eugênio <wesleyeugenio3@gmail.com>
2022-09-19 13:45:27 -0300

    funcionalidade de embaralhar array feita

--- src/controllers/players.js
+++ src/controllers/players.js
@@ -3,6 +3,12 @@ const knex = require("../connection");
 const getPlayers = async (req, res) => {
   const players = await knex("players");
 
+  function shuffleArray(players) {
+    players.sort(() => Math.random() - 0.5);
+  }
+
+  shuffleArray(players);
+
   return res.json(players);
 };
 
